9905f3429eb99a672000ea3b1d2ad5b7f8657d29
Azhi-Othman <azhiothman@gmail.com>
2021-12-09 11:06:08 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from "express";
+import userRouter from "./src/routes/user.routes.js";
+const app = express();
+
+//set up route
+
+app.use(userRouter);
+
+app.listen(5000, () => {
+  console.log("listing on http://localhost:5000");
+});
